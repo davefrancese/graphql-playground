58f3cdfbc73399319e0191973832166f388f881c
dfrancese <davefrancese@gmail.com>
2018-05-05 20:52:45 -0600

    connect to mongoDB

--- server/app.js
+++ server/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
+const mongoose = require('mongoose')
 
 const app = express();
 const port = 3000;
+
+mongoose.connect('mongodb://dave:test123@ds215380.mlab.com:15380/gql-playground');
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+})
 
 app.use('/graphql', graphqlHTTP({
   schema,
